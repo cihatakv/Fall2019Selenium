8dc28823cfffb04c5abe38666b36a01715e4099a
Cihat Akviranli <cihatakv@gmail.com>
2020-03-01 11:15:02 -0600

    first commit

---
+++ src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
@@ -0,0 +1,20 @@
+package com.automation.tests.day1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class MyFirstSeleniumScript {
+
+    public static void main(String[] args) throws InterruptedException {
+        //setup chromedriver
+        WebDriverManager.chromedriver().setup();
+        //create chromedriver object
+        ChromeDriver driver = new ChromeDriver();
+        //open some website
+        driver.get("http://google.com");
+        Thread.sleep(3000);
+
+    }
+}
