1c9256d437ba1a0b4cf002146d66fd4207b98ed8
Cihat Akviranli <cihatakv@gmail.com>
2020-03-01 19:52:34 -0600

    Basic Navigation in Selenium - Update

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -1,3 +1,4 @@
+
 package com.automation.tests.day2;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,13 +16,53 @@ public class BasicNavigation {
 
         Thread.sleep(3000);
         driver.get("https://www.google.com");
+        Thread.sleep(1000);
+        driver.manage().window().maximize();
+        String expectedTitle = "Google";
 
         String title = driver.getTitle();
 
-        System.out.println("title = " + title);
-        Thread.sleep(3000);
-        driver.close();
+        if (title.equals(expectedTitle)) {
+            System.out.println("TEST PASSED!");
+        } else {
+            System.out.println("TEST FAILED!");
+        }
 
+        driver.navigate().to("https://www.amazon.com");
+        if (driver.getTitle().toLowerCase().contains("amazon")) {
+            System.out.println("TEST PASSED!");
+        } else {
+            System.out.println("TEST FAILED!");
+        }
+        //comeback to google
+        driver.navigate().back();
+        Thread.sleep(3000);
+        verifyEquals(driver.getTitle(), "Google");
+
+        driver.navigate().forward();
+        Thread.sleep(3000);
+
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+        //checking if page title is equals to Google
+        //.getTitle() - returns page title
+        //must be at the end
+        driver.close();//to close browser
+        //browser cannot close itself
+    }
+
+    /**
+     * Check if to strings are same. If print TEST PASSED! message.
+     * Otherwise, print TEST FAILED message
+     *
+     * @param arg1
+     * @param arg2
+     */
+    public static void verifyEquals(String arg1, String arg2) {
+        if (arg1.equals(arg2)) {
+            System.out.println("TEST PASSED!");
+        } else {
+            System.out.println("TEST FAILED!");
+        }
 
     }
 }
