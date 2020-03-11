06b8504a5f377732f9aa4c21db4f57a57e0c306d
Cihat Akviranli <cihatakv@gmail.com>
2020-03-11 13:44:32 -0500

    Day 4 - findElementById by tagName practices added

---
+++ src/test/java/com/automation/tests/day4/VerifyThatElementIsGone.java
@@ -0,0 +1,32 @@
+package com.automation.tests.day4;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class VerifyThatElementIsGone {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().version("79").setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("disappearing_button")).click();
+
+        Thread.sleep(2000);
+
+        if (driver.findElements(By.id("disappearing_button")).size() == 0)
+            System.out.println("TEST PASSED");
+        else
+            System.out.println("TEST FAILED");
+
+        driver.quit();
+
+    }
+}
